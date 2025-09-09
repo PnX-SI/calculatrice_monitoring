6355ee7ae5c1c245e01d6a003811e2068fc49d7c
Marc-Antoine Dupré <marc-antoine.dupre@makina-corpus.com>
2025-09-09 11:36:21 +0200

    refactor: rename 1-letter variables

--- backend/calculatrice_monitoring/tests/test_routes.py
+++ backend/calculatrice_monitoring/tests/test_routes.py
@@ -5,14 +5,14 @@ from .fixtures import indicators  # noqa: F401  # We need to import fixtures so 
 
 
 def test_get_indicators_empty_list(client):
-    r = client.get(url_for("calculatrice.get_indicators"))
-    assert r.status_code == 200
-    assert r.json == []
+    response = client.get(url_for("calculatrice.get_indicators"))
+    assert response.status_code == 200
+    assert response.json == []
 
 
 @pytest.mark.usefixtures("indicators")
 def test_get_indicators(client):
-    r = client.get(url_for("calculatrice.get_indicators"))
-    assert r.status_code == 200
+    response = client.get(url_for("calculatrice.get_indicators"))
+    assert response.status_code == 200
     expected_names = ["Indicator0", "Indicator1", "Indicator2"]
-    assert [j["name"] for j in r.json] == expected_names
+    assert [indic["name"] for indic in response.json] == expected_names
