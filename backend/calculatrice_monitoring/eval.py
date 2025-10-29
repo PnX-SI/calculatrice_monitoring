35a5398526492ce5f5456df860209263da13a791
Marc-Antoine Dupré <marc-antoine.dupre@makina-corpus.com>
2025-10-29 10:37:29 +0100

    wip : ajout moyenne avec scope visit

--- backend/calculatrice_monitoring/eval.py
+++ backend/calculatrice_monitoring/eval.py
@@ -1,6 +1,8 @@
+from collections import defaultdict
+
 from geonature.app import create_app
 from gn_module_monitoring.config.repositories import get_config
-from gn_module_monitoring.monitoring.models import TMonitoringObservations
+from gn_module_monitoring.monitoring.models import TMonitoringObservations, TMonitoringSites, TMonitoringVisits
 
 app = create_app()
 
@@ -38,21 +40,32 @@ def getHE(cd_nom):
 config = get_config("mheo_flore_test")
 
 
+# class Site:
+#     def __init__(self, site_model_instance):
+#         self.id_base_site = site_model_instance.id_base_site
+#         self.base_site_name = site_model_instance.base_site_name
+#
+#
+# def get_site(site_id):
+#     site_model_instance = db.session.scalar(db.select(TMonitoringSites).filter(TMonitoringSites.id_base_site == site_id))
+#     return Site(site_model_instance)
+
+
+class Visit:
+    def __init__(self, visit_model_instance):
+        self.id_base_visit = visit_model_instance.id_base_visit
+        self.visit_date_min = visit_model_instance.visit_date_min
+
+
+def get_visit(visit_id):
+    visit_model_instance = db.session.scalar(db.select(TMonitoringVisits).filter(TMonitoringVisits.id_base_visit == visit_id))
+    return Visit(visit_model_instance)
+
+
 class Observation:
     def __init__(self, observation_model_instance):
         self.abondance = observation_model_instance.data["abondance"]
-
-
-def create_prop_collection_from_entities(instances, scope, property):
-    values = []
-    for instance in instances:
-        values.append(
-            PropertyValue(
-                value=getattr(instance, property),
-                entity=instance,
-            )
-        )
-    return PropertyCollection(values=values, scope=scope)
+        self.visit = get_visit(observation_model_instance.id_base_visit)
 
 
 class PropertyCollection:
@@ -74,6 +87,18 @@ class MonitoringCollection:
         self.scope = scope
 
 
+def create_prop_collection_from_entities(instances, scope, property):
+    values = []
+    for instance in instances:
+        values.append(
+            PropertyValue(
+                value=getattr(instance, property),
+                entity=instance,
+            )
+        )
+    return PropertyCollection(values=values, scope=scope)
+
+
 def get_observations():
     query = db.select(TMonitoringObservations)
     tmos = db.session.scalars(query).all()
@@ -92,18 +117,48 @@ def get_observation_collection():
 
 
 # TODO: ajouter un scope="site" !
-def Moyenne(prop_collection, scope_sites=False):
-    sum = 0
-    for prop_value in prop_collection.values:
-        sum += int(prop_value.value)
-    moyenne = sum / len(prop_collection.values)
-    return PropertyCollection(
-        values=[
-            PropertyValue(value=moyenne, entity="globale")
-        ],
-        scope="global"
-    )
+def Moyenne(prop_collection, scope_visit=False):
+    if not scope_visit:
+        sum = 0
+        for prop_value in prop_collection.values:
+            sum += int(prop_value.value)
+        moyenne = sum / len(prop_collection.values)
+        return PropertyCollection(
+            values=[
+                PropertyValue(value=moyenne, entity="globale")
+            ],
+            scope="global"
+        )
+    else:
+        # init sum_dict with site id as key
+        sum = defaultdict(lambda: 0)
+        # init length_dict with site id as key
+        length = defaultdict(lambda: 0)
+        visits = {}
+        # foreach propvalue:
+        for prop_value in prop_collection.values:
+            visit = prop_value.entity.visit
+            visit_id = visit.id_base_visit # TODO : link an observation's site
+            visits[visit_id] = visit
+            sum[visit_id] += int(prop_value.value)
+            length[visit_id] += 1
+        return PropertyCollection(
+            values=[
+                PropertyValue(
+                    value=sum[visit_id] / length[visit_id],
+                    entity=visit,
+                ) for visit_id, visit in visits.items()
+            ],
+            scope="visit"
+        )
 
+
+observations_collection = get_observation_collection()
+
+
+result = Moyenne(
+    observations_collection.abondance, scope_visit=True
+)
 
 
 code = """
@@ -127,8 +182,8 @@ assert context["moyenne"].values[0].value == 1.4102564102564104
 # - les PropertyValues ont les attributs value et site
 
 code = """
-moyenne = Moyenne(observations.abondance, scope_sites=True)
-labels = [v.site.base_site_name for v in moyenne.values]
+moyenne = Moyenne(observations.abondance, scope_visit=True)
+labels = [v.entity.visit_date_min for v in moyenne.values]
 values = [v.value for v in moyenne.values]
 """
 observations = get_observation_collection()
@@ -147,14 +202,16 @@ assert "moyenne" in context
 
 assert "labels" in context
 labels = context["labels"]
-for expected_label in [
-    "Transect 1 Quadrat 1",
-    "Transect 1 Quadrat 2",
-    "Transect 1 Quadrat 3",
-    "Transect 2 Quadrat 4",
-    "Transect 2 Quadrat 5",
-]:
-    assert expected_label in labels
+# for expected_label in [
+#     "Transect 1 Quadrat 1",
+#     "Transect 1 Quadrat 2",
+#     "Transect 1 Quadrat 3",
+#     "Transect 2 Quadrat 4",
+#     "Transect 2 Quadrat 5",
+# ]:
+#     assert expected_label in labels
+import datetime
+assert [datetime.date(2023, 5, 22)] * 5 == labels
 assert "values" in context
 values = context["values"]
 # from
