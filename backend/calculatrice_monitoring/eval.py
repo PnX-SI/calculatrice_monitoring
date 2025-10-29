34938912986cc5abe0b5607da00f7cae0d7ca263
Marc-Antoine Dupré <marc-antoine.dupre@makina-corpus.com>
2025-10-29 16:42:38 +0100

    wip : add code moyenne HE par site

--- backend/calculatrice_monitoring/eval.py
+++ backend/calculatrice_monitoring/eval.py
@@ -40,11 +40,66 @@ ctx.push()
 # - gérer plusieurs types de valeurs pour les MonitoringObjects
 # - start to look at way to report meaningful errors which happened in indicator's code
 # - extraire les variables créées sans les variables du contexte original
+# - faire un test de robustesse avec données manquantes (une observation sans abondance)
+
+
+# architecture :
+# - continuer à implémenter I02 pour voir => tableau de référence
+# - de vrais tests pytest avec fixtures
+# - factoriser les classes => Obs, Site, Visit, ... ==> Entity
+# - récupérer properties dynamiquement avec la config
+#   + ne pas prendre les propriétés cachées ?
+# - d'autres points d'entrée que 'observations'
 
 
 # TODO: function to emulate reference table lookup
-def getHE(cd_nom):
-    pass
+def get_he(cd_nom):
+    map = {
+        81610: 9,
+        89200: 5,
+        92501: 5,
+        95463: 6,
+        96271: 6,
+        98910: 7,
+        99373: 5,
+        100310: 6,
+        100387: 9,
+        102900: 5,
+        104173: 7,
+        105431: 10,
+        105966: 4,
+        106581: 5,
+        106918: 7,
+        107038: 8,
+        107117: 7,
+        112741: None,
+        112975: 7,
+        113260: 8,
+        115156: 5,
+        116759: 6,
+        119097: 5,
+        119585: 7,
+        119915: 8,
+        119948: 6,
+        119977: 5,
+        191232: None,
+    }
+    return map[cd_nom]
+
+
+def get_he_prop_collection(cd_nom_props):
+    he_values = []
+    for prop in cd_nom_props.values:
+        he_values.append(
+            PropertyValue(
+                value=get_he(prop.value),
+                entity=prop.entity,
+            )
+        )
+    return PropertyCollection(
+        values=he_values,
+        scope=cd_nom_props.scope
+    )
 
 
 # TODO: given the protocol create object with generic and specific properties
@@ -66,7 +121,8 @@ class Site:
 
 
 def get_site(site_id):
-    site_model_instance = db.session.scalar(db.select(TMonitoringSites).filter(TMonitoringSites.id_base_site == site_id))
+    site_model_instance = db.session.scalar(
+        db.select(TMonitoringSites).filter(TMonitoringSites.id_base_site == site_id))
     return Site(site_model_instance)
 
 
@@ -89,7 +145,7 @@ class Observation:
         self.abondance = observation_model_instance.data["abondance"]
         self.visit = get_visit(observation_model_instance.id_base_visit)
         self.site = get_site(self.visit.id_base_site)
-
+        self.cd_nom = observation_model_instance.cd_nom
 
 
 class PropertyCollection:
@@ -135,8 +191,10 @@ def get_observations():
 def get_observation_collection():
     observations = get_observations()
     abondance = create_prop_collection_from_entities(instances=observations, scope="observation", property="abondance")
+    cd_nom = create_prop_collection_from_entities(instances=observations, scope="observation", property="cd_nom")
     coll = MonitoringCollection(scope="observation")  # TODO: miss passing entities here
     coll.abondance = abondance
+    coll.cd_nom = cd_nom
     return coll
 
 
@@ -183,6 +241,9 @@ def Moyenne(prop_collection, scope="global"):
         lengths = defaultdict(lambda: 0)
         scope_entities = {}
         for prop_value in prop_collection.values:
+            if prop_value.value is None:
+                # FIXME: missing value => we should fail properly here
+                continue  # for now we just skip it
             scope_entity = getattr(prop_value.entity, scope)
             scope_entities[scope_entity.id] = scope_entity
             sums[scope_entity.id] += int(prop_value.value)
@@ -216,7 +277,6 @@ assert "moyenne" in context
 assert len(context["moyenne"].values) == 1
 assert context["moyenne"].values[0].value == 1.4102564102564104
 
-
 # ---------- TEST AVEC MOYENNE ABONDANCE POURCENTAGE TOUTES OBSERVATIONS -------------
 
 code = """
@@ -235,7 +295,6 @@ assert "moyenne" in context
 assert len(context["moyenne"].values) == 1
 assert context["moyenne"].values[0].value == 10.333333333333334
 
-
 # ---------- TEST AVEC MOYENNE ABONDANCE PAR VISITE -------------
 
 code = """
@@ -246,8 +305,12 @@ values = [v.value for v in moyenne.values]
 observations = get_observation_collection()
 global_context = dict()
 context = dict()
+
+
 def add_to_ctx(obj):
     global_context[obj.__name__] = obj
+
+
 for symbol in [
     Moyenne,
     PropertyValue,
@@ -277,7 +340,6 @@ for expected_value in [
             break
     else:
         raise AssertionError(f"{expected_value} not found in {values}")
-
 
 # ---------- TEST AVEC CONVERSION ABONDANCE EN POURCENTAGE -------------
 
@@ -310,8 +372,12 @@ values = [v.value for v in moyenne.values]
 observations = get_observation_collection()
 global_context = dict()
 context = dict()
+
+
 def add_to_ctx(obj):
     global_context[obj.__name__] = obj
+
+
 for symbol in [
     Moyenne,
     PropertyValue,
@@ -336,7 +402,6 @@ for expected_value in [7.0, 41.0, 10.0, 3.0, 9.285714285714286]:
     else:
         raise AssertionError(f"{expected_value} not found in {values}")
 
-
 # ---------- TEST AVEC MOYENNE ABONDANCE PAR SITE -------------
 
 code = """
@@ -347,8 +412,12 @@ values = [v.value for v in moyenne.values]
 observations = get_observation_collection()
 global_context = dict()
 context = dict()
+
+
 def add_to_ctx(obj):
     global_context[obj.__name__] = obj
+
+
 for symbol in [
     Moyenne,
     PropertyValue,
@@ -380,6 +449,55 @@ for expected_value in [
     1,
     1.3571428571428571,
 ]:
+    for value in values:
+        if abs(value - expected_value) < sys.float_info.epsilon:
+            break
+    else:
+        raise AssertionError(f"{expected_value} not found in {values}")
+
+# ---------- TEST AVEC MOYENNE HE PAR SITE -------------
+
+code = """
+valeurs_he = get_he_prop_collection(observations.cd_nom)
+moyenne = Moyenne(valeurs_he, scope="site")
+labels = [v.entity.base_site_name for v in moyenne.values]
+values = [v.value for v in moyenne.values]
+"""
+observations = get_observation_collection()
+global_context = dict()
+context = dict()
+
+
+def add_to_ctx(obj):
+    global_context[obj.__name__] = obj
+
+
+for symbol in [
+    Moyenne,
+    PropertyValue,
+    MonitoringCollection,
+    PropertyCollection,
+    get_he_prop_collection
+]:
+    add_to_ctx(symbol)
+global_context["observations"] = observations
+
+exec(code, global_context, context)
+
+assert "moyenne" in context
+assert "labels" in context
+labels = context["labels"]
+for expected_label in [
+    "Transect 1 Quadrat 1",
+    "Transect 1 Quadrat 2",
+    "Transect 1 Quadrat 3",
+    "Transect 2 Quadrat 4",
+    "Transect 2 Quadrat 5",
+]:
+    assert expected_label in labels
+assert "values" in context
+values = context["values"]
+for expected_value in [7.833333333333333, 7.5, 5.769230769230769, 6.5, 6.0]:
     for value in values:
         if abs(value - expected_value) < sys.float_info.epsilon:
             break
