68d4bc0f1297ae5764010e6b54719491b2ed2fe6
Marc-Antoine Dupré <marc-antoine.dupre@makina-corpus.com>
2025-10-31 09:53:58 +0100

    wip : format test_eval.py

--- backend/calculatrice_monitoring/tests/test_eval.py
+++ backend/calculatrice_monitoring/tests/test_eval.py
@@ -1,14 +1,17 @@
+# ruff: noqa: PLC2401  # (PLC2401=Function name contains a non-ASCII character)
+
 import datetime
 from decimal import Decimal
+
 import pytest
 
 from calculatrice_monitoring.eval import evaluate
 
 from .fixtures import (
-    # FIXME: is there a way no to import those "unused" fixtures
-    users,
-    protocols,
     monitoring_objects,
+    # FIXME: is there a way no to import those "unused" fixtures
+    protocols,
+    users,
 )
 
 
@@ -48,7 +51,7 @@ moyenne = Moyenne(observations.abondance, scope="visit")
         assert "moyenne" in variables
         properties = variables["moyenne"].values
         labels = [prop.entity.visit_date_min for prop in properties]
-        assert  labels == [datetime.date(2023, 5, 22)] * 5
+        assert labels == [datetime.date(2023, 5, 22)] * 5
         values = [prop.value for prop in properties]
         expected_values = [
             Decimal(1.3333333333333333),
@@ -56,7 +59,7 @@ moyenne = Moyenne(observations.abondance, scope="visit")
             Decimal(1.3571428571428571),
             Decimal(1),
             Decimal(1.3571428571428571),
-            ]
+        ]
         assert values == pytest.approx(expected_values)
 
     @pytest.mark.usefixtures("monitoring_objects")
@@ -73,11 +76,11 @@ moyenne = Moyenne(create_abondance_perc(observations), scope="visit")
         assert labels == [datetime.date(2023, 5, 22)] * 5
         values = [prop.value for prop in properties]
         expected_values = [
-            Decimal('7'),
-            Decimal('41.25'),
-            Decimal('10.07142857142857142857142857'),
-            Decimal('3'),
-            Decimal('9.321428571428571428571428571'),
+            Decimal("7"),
+            Decimal("41.25"),
+            Decimal("10.07142857142857142857142857"),
+            Decimal("3"),
+            Decimal("9.321428571428571428571428571"),
         ]
         assert values == pytest.approx(expected_values)
 
@@ -102,11 +105,11 @@ moyenne = Moyenne(observations.abondance, scope="site")
         assert labels == expected_labels
         values = [prop.value for prop in properties]
         expected_values = [
-            Decimal('1.333333333333333333333333333'),
-            Decimal('3'),
-            Decimal('1.357142857142857142857142857'),
-            Decimal('1'),
-            Decimal('1.357142857142857142857142857'),
+            Decimal("1.333333333333333333333333333"),
+            Decimal("3"),
+            Decimal("1.357142857142857142857142857"),
+            Decimal("1"),
+            Decimal("1.357142857142857142857142857"),
         ]
         assert values == pytest.approx(expected_values)
 
@@ -140,11 +143,11 @@ médiane = Médiane(moyenne)
         assert labels == expected_labels
         values = [prop.value for prop in properties]
         expected_values = [
-            Decimal('7.833333333333333333333333333'),
-            Decimal('7.5'),
-            Decimal('5.769230769230769230769230769'),
-            Decimal('6.5'),
-            Decimal('6'),
+            Decimal("7.833333333333333333333333333"),
+            Decimal("7.5"),
+            Decimal("5.769230769230769230769230769"),
+            Decimal("6.5"),
+            Decimal("6"),
         ]
         assert values == pytest.approx(expected_values)
 
@@ -179,10 +182,10 @@ médiane = Médiane(moyenne)
         assert labels == expected_labels
         values = [prop.value for prop in properties]
         expected_values = [
-            Decimal('8.785714285714285714285714286'),
-            Decimal('7.181818181818181818181818182'),
-            Decimal('5.684782608695652173913043478'),
-            Decimal('6.5'),
-            Decimal('5.4'),
+            Decimal("8.785714285714285714285714286"),
+            Decimal("7.181818181818181818181818182"),
+            Decimal("5.684782608695652173913043478"),
+            Decimal("6.5"),
+            Decimal("5.4"),
         ]
         assert values == pytest.approx(expected_values)
